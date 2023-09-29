40fc34bff11b1482825b7968b5df6b0e4d81ca93
Cumpri <rafaelcumpri.senai564@gmail.com>
2023-09-29 19:38:08 -0300

    feat : page.js toogle screen

--- app/page.js
+++ app/page.js
@@ -1,95 +1,56 @@
-import Image from 'next/image'
+'use client'
+
+import React from 'react'
+import { useState } from 'react'
+import pastrys from '@/data/pastryFull'
+import Pastry from '@/models/pastry'
+import PastrysList from '@/models/pastrysList'
 import styles from './page.module.css'
+import drinks from '@/data/drinks'
+
+const pastryMakedList = new PastrysList()
+const pastrysOrders = new PastrysList()
+
+pastrys.map(pastry => {
+  const pastryMaked = new Pastry(pastry.name, pastry.ingredients)
+  pastryMakedList.add(pastryMaked)
+})
 
 export default function Home() {
+
+  const [screenButtonStyle, setScreenButtonStyle] = useState(styles.showing)
+
+  const [makePastryOrderStyle, setmakePastryOrderStyle] = useState(styles.hidden)
+
+  const [pastrysOrdersStyle, setpastrysOrdersStyle] = useState(styles.hidden)
+
+  const changeScreenButtonToMakePastryOrder = () => {
+    setScreenButtonStyle(styles.hidden)
+    setmakePastryOrderStyle(styles.showing)
+  }
+
+  const changeScreenButtonToPastrysOrders = () => {
+    setScreenButtonStyle(styles.hidden)
+    setpastrysOrdersStyle(styles.showing)
+  }
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className={screenButtonStyle}>
+        <section className={styles.doubleSection} onClick={changeScreenButtonToMakePastryOrder}>
+          <h1>Faça um pedido</h1>
+        </section>
+        <section className={styles.doubleSection} onClick={changeScreenButtonToPastrysOrders}>
+          <h1>Veja os pedidos ativos</h1>
+        </section>
+      </main>
+      <main className={makePastryOrderStyle}>
+      </main>
+      <main className={pastrysOrdersStyle}>
+        {
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        }
+      </main>
+    </>
   )
 }

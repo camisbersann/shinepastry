77ea8102513b45b1577f75dbe8b0e8ca5f6db75a
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2023-09-29 14:35:27 -0300

    feat : create data with pastryIngredients

---
+++ data/pastryIngredients.js
@@ -0,0 +1,74 @@
+const pastryIngredients = [
+    {
+        id: 1,
+        name: 'Carne moída',
+        price: 10.00,
+    },
+    {
+        id: 2,
+        name: 'Chocolate',
+        price: 5.00,
+    },
+    {
+        id: 3,
+        name: 'Presunto',
+        price: 2.00,
+    },
+    {
+        id: 4,
+        name: 'Frango desfiado',
+        price: 10.00,
+    },
+    {
+        id: 5,
+        name: 'Bacon',
+        price: 5.00,
+    },
+    {
+        id: 6,
+        name: 'Palmito',
+        price: 1.00,
+    },
+    {
+        id: 7,
+        name: 'Tomate',
+        price: 1.00,
+    },
+    {
+        id: 8,
+        name: 'Cebola',
+        price: 1.00,
+    },
+    {
+        id: 9,
+        name: 'Azeitona',
+        price: 1.00,
+    },
+    {
+        id: 10,
+        name: 'Milho',
+        price: 1.00,
+    },
+    {
+        id: 11,
+        name: 'Ervilha',
+        price: 1.00,
+    },
+    {
+        id: 12,
+        name: 'Requeijão',
+        price: 1.00,
+    },
+    {
+        id: 13,
+        name: 'Catupiry',
+        price: 1.00,
+    },
+    {
+        id: 14,
+        name: 'Cheddar',
+        price: 1.00,
+    },
+]
+
+export default pastryIngredients
